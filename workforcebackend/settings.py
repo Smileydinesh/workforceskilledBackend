0890b9c53530f207dda628cf7803da30de1be3e4
smiley dinesh <smileydinesheee@gmail.com>
2026-01-10 13:10:22 +0530

    Add postgres + dotenv support

--- workforcebackend/settings.py
+++ workforcebackend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 from dotenv import load_dotenv
+import dj_database_url
 
 from pathlib import Path
 from datetime import timedelta
@@ -95,11 +96,13 @@ WSGI_APPLICATION = 'workforcebackend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=ENVIRONMENT == "production",
+    )
 }
+
 
 # CORS
 CORS_ALLOWED_ORIGINS = [
